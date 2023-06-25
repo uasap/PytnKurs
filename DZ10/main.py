37805876454c2dbdcda406ddd10789187bf03507
valery tryapitsyn <tryapitsyn@healbe.com>
2023-06-25 09:00:51 +0300

    DZ10

---
+++ DZ10/main.py
@@ -0,0 +1,27 @@
+'''| Задание 44 |
+| --- |
+| В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего из 1 столбца. Ваша задача перевести его в one hot вид. Сможете ли вы это сделать без get_dummies?
+
+
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI'lst})
+data.head() |'''
+
+
+import random
+import pandas as pd
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+print(lst)
+print()
+data = pd.DataFrame({'whoAmI':lst})
+data.loc[data['whoAmI'] == 'robot', 'robot_group'] = '1'
+data.loc[data['whoAmI'] != 'robot', 'robot_group'] = '0'
+data.loc[data['whoAmI'] == 'human', 'human_group'] = '1'
+data.loc[data['whoAmI'] != 'human', 'human_group'] = '0'
+
+data.head(n=20) 
